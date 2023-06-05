70c7b9750a060e156f93a627de9004885c8c504c
setouchi-h <setouchi.kazuki@gmail.com>
2023-06-05 17:44:12 +0900

    test

---
+++ test/StakeContract.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "../src/StakeContract.sol";
+import "./mocks/MockERC20.sol";
+import "forge-std/Test.sol";
+
+contract StakeContractTest is Test {
+    function setUp() public {}
+
+    function testExample() public {
+        assertTrue(true);
+    }
+}
