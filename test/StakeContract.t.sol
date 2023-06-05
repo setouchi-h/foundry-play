701b61fbeb21958aaeb01b6649a896f3703f44e6
setouchi-h <setouchi.kazuki@gmail.com>
2023-06-05 19:02:17 +0900

    a

--- test/StakeContract.t.sol
+++ test/StakeContract.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.18;
 
 import "../src/StakeContract.sol";
 import "./mocks/MockERC20.sol";
-import "forge-std/Test.sol";
+// import "forge-std/Test.sol";
 
 contract StakeContractTest is Test {
     StakeContract public stakeContract;
